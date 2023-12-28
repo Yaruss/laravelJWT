7966383bcb70be0de8f2dfce2949d2b8293894b3
Yarus <yaruss@gmail.com>
2023-12-28 17:55:03 +0300

    Install module JWT

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
 
+import viteCompression from 'vite-plugin-compression';
+
 export default defineConfig({
     plugins: [
+        viteCompression(),
         vue(),
         laravel({
             input: ['resources/scss/app.scss', 'resources/js/app.js'],
